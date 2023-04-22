4bf92ad03639f6bcb4f4909efbe719a615ffec8d
Technician <99316365+T3chn1ci4n@users.noreply.github.com>
2023-04-22 15:09:46 +0930

    Update fw.py

--- fw.py
+++ fw.py
@@ -3,137 +3,155 @@ from scapy.all import *
 import time
 import json
 
+# Tries to open file
 try:
-    f = open("firewallrules.json","r")
+    f = open("firewalls.json", 'r')
     y = json.load(f)
     f.close()
-    if("ListOfBannedIpAddr" in y):
-        if(type(y["ListOfBannedIpAddr"])==list):
-            ListOfBannedIpAddr = y["ListOfBannedIpAddr"]
+
+    # Check if ListOfBannedIpAddress exists in rule file
+    if ("ListOfBannedIpAddress" in y):
+        if (type(y["ListOfBannedIpAddress"])==list):
+            ListOfBannedIpAddress = y["ListOfBannedIpAddress"]
         else:
-            print("Invalid ListOfBannedIpAddr in rule file. Defaulting to []")
-            ListOfBannedIpAddr = []
+            print("Invalid ListOfBannedIpAddress in rule file. Setting to default []")
+            ListOfBannedIpAddress = []
     else:
-        print("ListOfBannedIpAddr missing in rule file. Defaulting to []")
-        ListOfBannedIpAddr = []
-            
-    if("ListOfBannedPorts" in y):
-        if(type(y["ListOfBannedPorts"])==list):
+        print("ListOfBannedIpAddress missing in rule file. Setting to default []")
+        ListOfBannedIpAddress = []
+
+    # Check if ListOfBannedPorts exists in rule file
+    if ("ListOfBannedPorts" in y):
+        if (type(y["ListOfBannedPorts"])==list):
             ListOfBannedPorts = y["ListOfBannedPorts"]
         else:
-            print("Invalid ListOfBannedPorts in rule file. Defaulting to []")
+            print("Invalid ListOfBannedPorts in rule file. Setting to default []")
             ListOfBannedPorts = []
     else:
-        print("ListOfBannedPorts missing in rule file. Defaulting to []")
+        print("ListOfBannedPorts missing in rule file. Setting to default []")
         ListOfBannedPorts = []
-            
-    if("ListOfBannedPrefixes" in y):
-        if(type(y["ListOfBannedPrefixes"])==list):
+
+    # Check if ListOfBannedPrefixes exists in rule file
+    if ("ListOfBannedPrefixes" in y):
+        if (type(y["ListOfBannedPrefixes"])==list):
             ListOfBannedPrefixes = y["ListOfBannedPrefixes"]
         else:
-            print("Invalid ListOfBannedPrefixes in rule file. Defaulting to []")
+            print("Invalid ListOfBannedPrefixes in rule file. Setting to default []")
             ListOfBannedPrefixes = []
     else:
-        print("ListOfBannedPrefixes missing in rule file. Defaulting to []")
+        print("ListOfBannedPrefixes missing in rule file. Setting to default []")
         ListOfBannedPrefixes = []
 
-    if("TimeThreshold" in y):
+    # Check if TimeThreshold exists in rule file
+    if ("TimeThreshold" in y):
         if(type(y["TimeThreshold"])==int):
             TimeThreshold = y["TimeThreshold"]
         else:
-            print("Invalid TimeThreshold in rule file. Defaulting to 10")
+            print("Invalid time threshold in rule file. Setting to default 10")
             TimeThreshold = 10
-    else:
-        print("TimeThreshold missing in rule file. Defaulting to 10")
+    else: 
+        print("TimeThreshold missing in rule file. Setting to default 10")
         TimeThreshold = 10
 
-    if("PacketThreshold" in y):
+    # Check if PacketThreshold exists in rule file
+    if ("PacketThreshold" in y):
         if(type(y["PacketThreshold"])==int):
             PacketThreshold = y["PacketThreshold"]
         else:
-            print("Invalid PacketThreshold in rule file. Defaulting to 100")
+            print("Invalid time threshold in rule file. Setting to default 100")
             PacketThreshold = 100
-    else:
-        print("PacketThreshold missing in rule file. Defaulting to 100")
+    else: 
+        print("PacketThreshold missing in rule file. Setting to default 100")
         PacketThreshold = 100
 
-    if("BlockPingAttacks" in y):
-        if(y["BlockPingAttacks"]=="True" or y["BlockPingAttacks"]=="False"):
-            BlockPingAttacks = eval(y["BlockPingAttacks"])
+    # Check if BlockPingAttacks exists in rule file
+    if ("BlockPingAttacks" in y):
+        if(type(y["BlockPingAttacks"])==int):
+            BlockPingAttacks = y["BlockPingAttacks"]
         else:
-            print("Invalid BlockPingAttacks in rule file. Defaulting to True")
+            print("Invalid block ping attack in rule file. Setting to default True")
             BlockPingAttacks = True
     else:
-        print("BlockPingAttacks missing in rule file. Defaulting to True")
+        print("Invalid block ping attack in rule file. Setting to default True")
         BlockPingAttacks = True
 
+    
+    
+# Will run if file doesn't exist
 except FileNotFoundError:
-    print("Rule file (firewallrules.json) not found, setting default values")
-    ListOfBannedIpAddr = [] 
-    ListOfBannedPorts = []
+    print("Json file was not found, Default Settings Initiated")
+    # List of Banned Ip/Port/Prefixes
+    ListOfBannedIpAddress = []
+    ListOfBannedPort = []
     ListOfBannedPrefixes = []
-    TimeThreshold = 10 #sec
-    PacketThreshold = 100    
+    # Threshold for packet/timeout
+    TimeThreshold = 10 #Seconds
+    PacketThreshold = 100
     BlockPingAttacks = True
 
+# Function for the firewall
 def firewall(pkt):
-	sca = IP(pkt.get_payload())
+    sca = IP(pkt.get_payload())
 
-	if(sca.src in ListOfBannedIpAddr):
-		print(sca.src, "is a incoming IP address that is banned by the firewall.")
-		pkt.drop()
-		return 
+    # Check if IP address is in banned list
+    if (sca.src in ListOfBannedIpAddress):
+        print(f"{sca.src} is incoming IP address that is banned by firewall.")
+        pkt.drop()
+        return
 
-	if(sca.haslayer(TCP)):
-		t = sca.getlayer(TCP)
-		if(t.dport in ListOfBannedPorts):
-			print(t.dport, "is a destination port that is blocked by the firewall.")
-			pkt.drop()
-			return 
+    # Check if TCP port is in banned list
+    if (sca.haslayer(TCP)):
+        t = sca.getlayer(TCP)
+        if(t.dport in ListOfBannedPorts):
+            print(f"{sca.dport} is a destination port that is banned by firewall")
+            pkt.drop()
+            return
 
-	if(sca.haslayer(UDP)):
-		t = sca.getlayer(UDP)
-		if(t.dport in ListOfBannedPorts):
-			print(t.dport, "is a destination port that is blocked by the firewall.")
-			pkt.drop()
-			return 
+    # Check if UDP port is in banned list
+    if (sca.haslayer(UDP)):
+        t = sca.getlayer(UDP)
+        if(t.dport in ListOfBannedPorts):
+            print(f"{t.dport} is destination port that is banned by firewall firewall")
+            pkt.drop()
+            return
+    # Check if Prefix is in banned list
+    if (True in [sca.src.find(suff)==0 for suffix in ListOfBannedPrefixes]):
+        print(f"Prefix of sca.src is banned by firewall.")
+        pkt.drop()
+        return
+    # Prevention for flood attacks
+    if (BlockPingAttacks and sca.haslayer(ICMP)):
+        t = sca.getlayer(ICMP)
+        if (t.code==0):
+            if(sca.src in DictOfPackets):
+                # Detects if there is a flood attacks by checking value of threshold
+                temptime = list(DictOfPackets[sca.src])
+                if (len(DictOfPackets[sca.src]) >= PacketThreshold):
+                    if (time.time() - DictOfPackets[sca.src][0] <= TimeThreshold):
+                        print(f"Ping by %s blocked by firewall (too many requests in short span of time)." %(sca.src)) 
+                        pkt.drop()
+                        return
+                    else:
+                        DictOfPackets[sca.src].pop(0)
+                        DictOfPackets[sca.src].append(time.time())
+                else:
+                    DictOfPackets[sca.src].append(time.time())
+            else:
+                DictOfPackets[sca.src] = [time.time()]
 
-	if(True in [sca.src.find(suff)==0 for suff in ListOfBannedPrefixes]):
-		print("Prefix of " + sca.src + " is banned by the firewall.")
-		pkt.drop()
-		return
+        print("Packet from %s accepted and forwarded to IPTABLES" %(sca.src))
+        pkt.accept()
+        return
 
-	if(BlockPingAttacks and sca.haslayer(ICMP)): #attempt at preventing hping3
-		t = sca.getlayer(ICMP)
-		if(t.code==0):
-			if(sca.src in DictOfPackets):
-				temptime = list(DictOfPackets[sca.src])
-				if(len(DictOfPackets[sca.src]) >= PacketThreshold):
-					if(time.time()-DictOfPackets[sca.src][0] <= TimeThreshold):
-						print("Ping by %s blocked by the firewall (too many requests in short span of time)." %(sca.src))
-						pkt.drop()
-						return
-					else:
-						DictOfPackets[sca.src].pop(0)
-						DictOfPackets[sca.src].append(time.time())
-				else:
-					DictOfPackets[sca.src].append(time.time())
-			else:
-				DictOfPackets[sca.src] = [time.time()]
+    print("Packet from %s accepted and forwarded to IPTABLES" %(sca.src))
 
-		#print("Packet from %s accepted and forwarded to IPTABLES" %(sca.src))		
-		pkt.accept()
-		return 
-	
-	#print("Packet from %s accepted and forwarded to IPTABLES" %(sca.src)) #commented coz its annoying
-	pkt.accept()
-
+# Filters the packets
 nfqueue = NetfilterQueue()
 nfqueue.bind(1,firewall)
 
-try:
-    nfqueue.run()
-except KeyboardInterrupt:
-	pass
+# will try to run 
+try: nfqueue.run()
+# will exit if key if press
+except KeyboardInterrupt: pass
 
 nfqueue.unbind()
